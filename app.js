cc7c79640adef0ca5c7e6c510dbb82b812d6d6cb
usonkrap <usonkrap@gmail.com>
2019-01-26 23:41:31 +0900

    Routing

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ import morgan from "morgan";  // for logging
 import helmet from "helmet";  // for secure
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = "4000";
-
-const handleListening = () => console.log(`Listen on : http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello my ass");
 const handlePrifile = (req, res) => res.send("Hello profile");
@@ -17,16 +14,11 @@ const handlePrifile = (req, res) => res.send("Hello profile");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(morgan("dev"));
 app.use(helmet());
 
 app.get("/", handleHome);
-
 app.get("/profile", handlePrifile);
+app.use("/user", userRouter);
 
-app.listen(PORT, handleListening);
-
-// app.get('/', function (req, res) {
-//   res.send('hello world')
-// })
+export default app;
