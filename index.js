d8390191faf2b3d759a60a74d17a77e0eb6e0aa5
usonkrap <usonkrap@gmail.com>
2019-01-26 22:01:26 +0900

    Test Middleware

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ const handleListening = () => console.log(`Listen on : http://localhost:${PORT}`
 const handleHome = (req, res) => res.send("Hello my ass");
 const handlePrifile = (req, res) => res.send("Hello profile");
 
+const betweenMid = (req, res, next) => {
+  console.log("between");
+  next();
+}
+
+app.use(betweenMid);
 
 app.get("/", handleHome)
+// app.get("/",betweenMid, handleHome)
+
 app.get("/profile", handlePrifile)
 
 app.listen(PORT, handleListening);
