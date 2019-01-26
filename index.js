465127b4ae14835061ba14b6635577041ce157f9
usonkrap <usonkrap@gmail.com>
2019-01-26 14:13:33 +0900

    Test Express Server and Route

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hi!")
+const express = require('express');
+const app = express();
+
+const PORT = "4000";
+
+function handleListening() {
+  console.log(`Listen on : http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  res.send("Hello home");
+}
+function handlePrifile(req, res) {
+  res.send("Hello profile");
+}
+
+
+app.get("/", handleHome)
+app.get("/profile", handlePrifile)
+
+app.listen(PORT, handleListening);
+
+// app.get('/', function (req, res) {
+//   res.send('hello world')
+// })
