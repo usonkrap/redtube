9f699eb6e92db8c8cb24abda2a86492dfc367baa
usonkrap <usonkrap@gmail.com>
2019-02-04 22:09:32 +0900

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,32 @@
 export const home = (req, res) => res.render("home", {
     pageTitle: "Home"
 });
-export const search = (req, res) => res.send("search");
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("video detal");
-export const editVideo = (req, res) => res.send("edit video");
-export const deleteVideo = (req, res) => res.send("delete video");
+
+export const search = (req, res) => {
+    // const searchingBy = req.query.term
+    const {
+        query: {
+            term: searchingBy
+        }
+    } = req;
+    res.render("search", {
+        pageTitle: "Search",
+        searchingBy
+    });
+}
+
+export const videos = (req, res) => res.render("videos", {
+    pageTitle: "Videos"
+});
+export const upload = (req, res) => res.render("upload", {
+    pageTitle: "Upload"
+});
+export const videoDetail = (req, res) => res.render("video detal", {
+    pageTitle: "videoDetail"
+});
+export const editVideo = (req, res) => res.render("edit video", {
+    pageTitle: "editVideo"
+});
+export const deleteVideo = (req, res) => res.render("delete video", {
+    pageTitle: "deleteVideo"
+});
