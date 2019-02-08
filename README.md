93ec824221e56bf18fc22bb6bee24a0ccc9c96c3
usonkrap <usonkrap@gmail.com>
2019-02-09 00:14:32 +0900

    Login and User Profile Controller

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Cloning Youtube with Vanilla and Node
 
 # Pages
 
-- [ ] Home
-- [x] Join
-- [x] Login
-- [x] Search
-- [ ] User Detail
-- [ ] Edit Profile
-- [ ] Change Password
-- [ ] Upload
-- [ ] Video Detail
-- [ ] Edit Video
+-   [ ] Home
+-   [x] Join
+-   [x] Login
+-   [x] Search
+-   [ ] User Detail
+-   [x] Edit Profile
+-   [x] Change Password
+-   [x] Upload
+-   [ ] Video Detail
+-   [x] Edit Video
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,16 +1,18 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { login, logout, getJoin, postJoin } from "../controllers/userController";
+import { login, logout, getJoin, postJoin, getLogin, postLogin } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
