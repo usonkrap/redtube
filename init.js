05d1b3ac566423cc422ddb152eee0b258fb29bc7
usonkrap <usonkrap@gmail.com>
2019-02-09 04:23:54 +0900

    set mongoDB and dotEnv

--- init.js
+++ init.js
@@ -1,6 +1,9 @@
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = "4000";
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`✅ Listen on : http://localhost:${PORT}`);
 
