ba59ad19a06014a2be315854fa34f1be56277a1a
usonkrap <usonkrap@gmail.com>
2019-02-09 14:26:02 +0900

    Uploading and Creating a Video

--- app.js
+++ app.js
@@ -13,13 +13,10 @@ const app = express();
 
 app.use(helmet());
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
 
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,15 @@
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+    try {
+        const videos = await Video.find({});
+        res.render("home", { pageTitle: "Home", videos });
+    } catch (error) {
+        console.log(error);
+        res.render("home", { pageTitle: "Home", videos: [] });
+    }
+};
 
 export const search = (req, res) => {
     // const searchingBy = req.query.term
@@ -11,13 +20,18 @@ export const search = (req, res) => {
 };
 
 export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
-export const postUpload = (req, res) => {
-    res.body;
+export const postUpload = async (req, res) => {
     const {
-        body: { file, title, description }
+        body: { title, description },
+        file: { path }
     } = req;
-    // TODO: upload and save video
-    res.redirect(routes.videoDetail(324393));
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    });
+    console.log(newVideo);
+    res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "videoDetail" });
