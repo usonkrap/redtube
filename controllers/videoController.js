b50162e8974608c83424c30ddbfcbd174fc8fde4
usonkrap <usonkrap@gmail.com>
2019-02-10 12:22:19 +0900

    Searching Videos with regex

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -11,12 +11,18 @@ export const home = async (req, res) => {
   }
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   // const searchingBy = req.query.term
   const {
     query: { term: searchingBy },
   } = req;
-  res.render('search', { pageTitle: 'Search', searchingBy });
+  let videos = [];
+  try {
+    videos = await Video.find({ title: { $regex: searchingBy, $options: 'i' } });
+  } catch (error) {
+    console.log(error);
+  }
+  res.render('search', { pageTitle: 'Search', searchingBy, videos });
 };
 
 export const getUpload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
