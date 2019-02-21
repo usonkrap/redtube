eec9e892665cdab9c366fcca22b253bc0fca322b
PARK Nosu <usonkrap@gmail.com>
2019-02-21 21:30:07 +0900

    Adding a Comment by API

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from '../routes';
 import Video from '../models/Video';
+import Comment from '../models/Comment';
 
 export const home = async (req, res) => {
   try {
@@ -47,7 +48,9 @@ export const videoDetail = async (req, res) => {
     params: { id },
   } = req;
   try {
-    const video = await Video.findById(id).populate('creator');
+    const video = await Video.findById(id)
+      .populate('creator')
+      .populate('comments');
     res.render('videoDetail', { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
@@ -108,6 +111,29 @@ export const postRegisterView = async (req, res) => {
     video.views += 1;
     video.save();
     res.status(200);
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
+
+// Add Comment
+
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+    user,
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id,
+    });
+    video.comments.push(newComment.id);
+    video.save();
   } catch (error) {
     res.status(400);
   } finally {
--- routes.js
+++ routes.js
@@ -30,6 +30,7 @@ const FACEBOOK_CALLBACK = '/auth/facebook/callback';
 // API
 const API = '/api';
 const REGISTER_VIEW = '/:id/view';
+const ADD_COMMENT = '/:id/comment';
 
 const routes = {
   home: HOME,
@@ -73,6 +74,7 @@ const routes = {
   facebookCallback: FACEBOOK_CALLBACK,
   api: API,
   registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT,
 };
 
 export default routes;
